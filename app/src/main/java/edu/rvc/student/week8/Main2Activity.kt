f4540a8064dac023133247e12a45a9c41a1e8601
Chuck Konkol <ckonkol@gmail.com>
2019-01-21 13:04:55 -0600

    Update Main2Activity.kt

--- app/src/main/java/edu/rvc/student/week8/Main2Activity.kt
+++ app/src/main/java/edu/rvc/student/week8/Main2Activity.kt
@@ -1,9 +1,11 @@
 package edu.rvc.student.week8
 
+import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -39,6 +41,26 @@ class Main2Activity : AppCompatActivity() {
         btnBack.setOnClickListener(View.OnClickListener {
             this.finish()
         })
+
+        //Fire hidekeyboard when user taps outside any text object
+//Place below code right before last right bracket in function onCreate
+        findViewById<View>(android.R.id.content).setOnTouchListener { _, event ->
+            hideKeyboard()
+            false
+        }
+
+    }
+
+    //function to hide keyboard goes right before the last right bracket of Class MainActivity
+//should auto import android.content.Context
+//should auto add import android.view.inputmethod.InputMethodManager
+    fun hideKeyboard() {
+        try {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        } catch (e: Exception) {
+            // TODO: handle exception
+        }
 
     }
 }
